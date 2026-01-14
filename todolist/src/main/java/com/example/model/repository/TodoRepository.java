66135b60d8ed26d8efe6594001415f649c2fcd9f
christmknd <christmokonda@gmail.com>
2026-01-14 11:15:30 +0100

    correct TodoRepository

--- todolist/src/main/java/com/example/model/repository/TodoRepository.java
+++ todolist/src/main/java/com/example/model/repository/TodoRepository.java
@@ -1,5 +1,9 @@
 package com.example.model.repository;
 
-public interface TodoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.model.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 
 }
